0f2d4f1b2845da8a713c2cf8244fdd6ba0c842fd
Peter Teuben <teuben@gmail.com>
2024-12-06 10:11:46 +0000

    new data

--- mk_runs.py
+++ mk_runs.py
@@ -10,15 +10,23 @@ project="2024-S1-MX-35"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
+
+on["BP_Psc"]         = [ 124679, 124681, 124683,]
+
+
 on["TYC_2597_735_1"] = [ 112866, 112867,]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
+pars1["BP_Psc"] = ""
+
 pars1["TYC_2597_735_1"] = ""
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
 pars2 = {}
+pars2["BP_Psc"]         = "pix_list=-13"
+
 pars2["TYC_2597_735_1"] = "pix_list=-13"
 
 #        common parameters per source on subsequent runs (run1c, run2c), e.g. bank=1 for WARES
