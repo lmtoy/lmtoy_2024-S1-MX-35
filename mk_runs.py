a2761d92b875aab7c6cfa96e254752affe642b30
Peter Teuben <teuben@gmail.com>
2025-08-19 01:23:34 +0000

    add no_valid_data

--- mk_runs.py
+++ mk_runs.py
@@ -11,22 +11,23 @@ project="2024-S1-MX-35"
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
 
-on["BP_Psc"]         = [ 124679, 124681, 124683,]
+on["BP_Psc"] = \
+ [ 124679, 124681, 124683, 138357, 138359, 138361, 138367, 138582, 138584, 138586, 139032, 
+   139034, 139036, 139038, 139042, 139148, 139150, 139152, 139179, 139181, 139183,]
 
-
-on["TYC_2597_735_1"] = [ 112866, 112867,]
+on["TYC_2597_735_1"] = \
+ [ 112866, 112867, 136799, 136800, 136812, 136813, 136821, 136822,-137008, 138269,-138270, 
+   138302, 138303, 138311, 138312, 138538, 138539, 138548, 138549,]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 pars1["BP_Psc"] = ""
-
 pars1["TYC_2597_735_1"] = ""
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
 pars2 = {}
 pars2["BP_Psc"]         = "pix_list=-13"
-
 pars2["TYC_2597_735_1"] = "pix_list=-13"
 
 #        common parameters per source on subsequent runs (run1c, run2c), e.g. bank=1 for WARES
